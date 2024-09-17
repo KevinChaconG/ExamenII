5d74d031e1ab50210482a4c504c93c944f94416b
Kevin Chacon <kevin27chacon@gmail.com>
2024-09-16 19:40:20 -0600

    Add project files.

---
+++ ExamenII/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenII
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenII/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenII
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
