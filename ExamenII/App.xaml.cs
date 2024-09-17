90e5630fe847ac90044369147dfbce58ae83c72a
Kevin Chacon <kevin27chacon@gmail.com>
2024-09-16 20:42:15 -0600

    se actualizó el MainPage de Navegacion

--- ExamenII/App.xaml.cs
+++ ExamenII/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ExamenII
+﻿using ExamenII.Views;
+
+namespace ExamenII
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new DescuentosView());
         }
     }
 }
--- ExamenII/Views/DescuentosView.xaml.cs
+++ ExamenII/Views/DescuentosView.xaml.cs
@@ -1,9 +1,15 @@
+using ExamenII.ViewModels;
+using ExamenII.Views;
+
 namespace ExamenII.Views;
 
 public partial class DescuentosView : ContentPage
 {
+	private DescuentosViewModel ViewModel;
 	public DescuentosView()
 	{
 		InitializeComponent();
+		ViewModel = new DescuentosViewModel();
+		BindingContext = ViewModel;
 	}
 }
